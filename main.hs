accf757f5e42f09ab2e7b7e7322a58722a0c7b51
spp-plotnikov <Sasha-Plotnikov-Production@yandex.ru>
2017-04-19 04:47:02 +0300

    added new function from excercise from stepik

--- main.hs
+++ main.hs
@@ -8,3 +8,28 @@ fibonacci n | n == 0 = 0
 
 helper cur pr n | n == 1 = cur
                 | otherwise = helper (pr + cur) cur (n - 1)
+
+
+seqA :: Integer -> Integer
+seqA n 
+	| n == 0 = 1
+	| n == 1 = 2
+	| n == 2 = 3
+	| n >= 3  = let
+			helper cur pr prpr n 
+				| n == 2 = cur
+				| otherwise = helper (cur + pr - 2 * prpr) cur pr (n - 1)
+		in 
+		 	helper 3 2 1 n
+	| otherwise = error "not positive"
+
+
+sum'n'count :: Integer -> (Integer, Integer)
+sum'n'count x
+	| x == 0 = (0, 1)
+	| otherwise = helper (abs x) (0, 0)
+		where
+			helper x (y, z)
+				| x == 0 = (y, z)
+				| otherwise = helper (x `div` 10) (y + x `mod` 10, z + 1)
+
