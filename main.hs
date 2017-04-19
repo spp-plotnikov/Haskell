49b4867454e48a6e0e1907977ae20118d835fa3f
spp-plotnikov <Sasha-Plotnikov-Production@yandex.ru>
2017-04-19 03:25:22 +0300

    added fibonacci

--- main.hs
+++ main.hs
@@ -1,2 +1,10 @@
-:l factorial
-foo 5
+module Fib where
+
+fibonacci :: Integer -> Integer
+fibonacci n | n == 0 = 0
+            | n == 1 = 1
+            | n > 1 = helper 1 0 n
+            | otherwise = (-1)^(1 - n) * fibonacci (-n)
+
+helper cur pr n | n == 1 = cur
+                | otherwise = helper (pr + cur) cur (n - 1)
